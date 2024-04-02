72296af22e1a158915aa609973c0494a3baca771
yaryna-bashchak <yaryna.bashchak@gmail.com>
2024-04-02 17:03:27 +0300

    get weather by city

--- lab4/index.js
+++ lab4/index.js
@@ -1,24 +1,60 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+const express = require('express')
+const axios = require('axios')
+const app = express()
+const PORT = 3000
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs')
+app.set('views', __dirname + '/views')
 
 app.get('/', (req, res) => {
-    res.send('Hello, Express');
-});
+  res.send('Hello, Express')
+})
 
 app.listen(PORT, () => {
-    console.log(`Сервер запущено на порту ${PORT}`);
-});
+  console.log(`Сервер запущено на порту ${PORT}`)
+})
 
 app.get('/weather', (req, res) => {
-    res.render('index');
-});
+  res.render('index')
+})
 
-app.get('/weather/:city', (req, res) => {
-    const city = req.params.city;
+app.get('/weather/current', async (req, res) => {
+  const { lat, lon } = req.query
+  const apiKey = 'fdd36c35ca1fc792abf77dce39c76a0c'
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=uk`
 
-    res.send(`Погода в місті ${city}: сонячно, +23°C`);
-});
+  try {
+    const response = await axios.get(url)
+    const { data } = response
+    res.render('city_weather', {
+      city: data.name,
+      temperature: data.main.temp,
+      description: data.weather[0].description,
+      humidity: data.main.humidity,
+      pressure: data.main.pressure
+    })
+  } catch (error) {
+    console.error(error)
+    res.send(`Помилка при отриманні даних про погоду: ${error}`)
+  }
+})
+
+app.get('/weather/:city', async (req, res) => {
+  const city = req.params.city
+  const apiKey = 'fdd36c35ca1fc792abf77dce39c76a0c'
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=uk`
+
+  try {
+    const response = await axios.get(url)
+    const { weather, main } = response.data
+    res.render('city_weather', {
+      city: city,
+      temperature: main.temp,
+      description: weather[0].description,
+      humidity: main.humidity,
+      pressure: main.pressure
+    })
+  } catch (error) {
+    res.send(`Помилка при отриманні даних про погоду: ${error}`)
+  }
+})
