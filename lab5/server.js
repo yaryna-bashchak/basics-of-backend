e6de1a2ea3569fe37decff6b96b3e68080d8a7b3
yaryna-bashchak <yaryna.bashchak@gmail.com>
2024-04-08 20:08:03 +0300

    create pages for get all, create and edit posts

--- lab5/server.js
+++ lab5/server.js
@@ -1,16 +1,39 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const postRoutes = require('./routes/postRoutes');
-const app = express();
-const port = 3000;
+const express = require('express')
+const mongoose = require('mongoose')
+const { engine } = require('express-handlebars')
+const postRoutes = require('./routes/postRoutes')
+const moment = require('moment')
+const app = express()
+const port = 3000
 
-app.use(express.json());
-app.use('/posts', postRoutes);
+app.engine('hbs', engine({
+  extname: '.hbs',
+  defaultLayout: 'main',
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+    allowProtoMethodsByDefault: true
+  },
+  helpers: {
+    formatDate: function (date, format) {
+      return moment(date).format(format)
+    }
+  }
+})
+)
+app.set('view engine', 'hbs')
+app.set('views', './views')
 
-mongoose.connect('mongodb+srv://bashchakyaryna:wUDknwFCy29zUlCK@cluster0.srklcrx.mongodb.net/?retryWrites=true&w=majority&appName=cluster0')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/posts', postRoutes)
+
+mongoose
+  .connect(
+    'mongodb+srv://bashchakyaryna:wUDknwFCy29zUlCK@cluster0.srklcrx.mongodb.net/?retryWrites=true&w=majority&appName=cluster0'
+  )
   .then(() => console.log('Successfully connected to MongoDB'))
-  .catch(err => console.error('Connection error', err));
+  .catch(err => console.error('Connection error', err))
 
 app.listen(port, () => {
-console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
